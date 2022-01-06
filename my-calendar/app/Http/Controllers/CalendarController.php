c00aa6122e04b9d5d10068e1c1a7a9f65e5918c1
daiki <foxmans1902@gmail.com>
2022-01-06 21:56:09 +0900

    カレンダーコントローラー、イベントコントローラーの作成

---
+++ my-calendar/app/Http/Controllers/CalendarController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Calendar;
+use Illuminate\Http\Request;
+
+class CalendarController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Calendar::all());
+    }
+
+    public function show(int $id)
+    {
+        return response()->json(Calendar::find($id));
+    }
+
+
+    public function create(Request $request)
+    {
+        $calendar = new Calendar();
+
+        return $this->_saveCalendar($request, $calendar);
+    }
+
+    public function save(Request $request)
+    {
+        $calendar = Calendar::find($request->id);
+
+        return $this->_saveCalendar($request, $calendar);
+    }
+
+    public function destroy(Request $request)
+    {
+        $calendar = Calendar::find($request->id);
+
+        if ($calendar->delete()) {
+            return response()->json($calendar);
+        } else {
+            return response()->json(['error', 'Delete Error']);
+        }
+    }
+
+    private function _saveCalendar(Request $request, $calendar)
+    {
+        $calendar->name = $request->input('name');
+        $calendar->color = $request->input('color');
+        $calendar->visibility = $request->input('visibility');
+        $calendar->user_id = $request->input('user_id');
+
+        if ($calendar->save()) {
+            return response()->json($calendar);
+        } else {
+            return response()->json(['error' => 'Save Error']);
+        }
+    }
+}
