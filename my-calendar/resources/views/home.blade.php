ecd916f085bbacd82a66c86461f1b986d02a6f6d
daiki <foxmans1902@gmail.com>
2022-01-10 11:41:45 +0900

    コードフォーマッターの導入と動作確認

--- my-calendar/resources/views/home.blade.php
+++ my-calendar/resources/views/home.blade.php
@@ -17,6 +17,10 @@
                     {{ __('You are logged in!') }}
                 </div>
             </div>
+            
+            　　　<div id="app">
+                    <example-component></example-component>
+                　</div>
         </div>
     </div>
 </div>
